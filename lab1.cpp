207e479471c9c1234792171e4bd2f3f714548e17
lakuzn <91047299+lakuzn@users.noreply.github.com>
2023-04-21 21:44:29 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,57 @@
+﻿#include "lab1.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	TList L;
+	int ord = 1;
+	int elem, index;
+	while (ord != 0) {
+		cout << "Введите номер команды: " << endl << "1 — добавить элемент в начало списка" << endl << "2 — добавить элемент в конец списка" << endl << "3 — добавить элемент в заданную позицию" << endl << "4 — удалить элемент по значению" << endl << "5 — удалить элемент по номеру позиции" << endl << "6 — найти элемент по значению" << endl << "7 — найти элемент в заданной позиции" << endl << "8 — вывод всех элементов списка" << endl << "0 — завершить исполнение программы" << endl;
+		cout << "Номер необходимой операции — ";
+		cin >> ord;
+		if (ord == 1) {
+			cout << "Какой элемент вы желаете добавить в начало списка?" << endl;
+			cin >> elem;
+			if (L.is_empty()) L.push(elem);
+			else L.push1(elem);
+		}
+		if (ord == 2) {
+			cout << "Какой элемент вы желаете добавить в конец списка?" << endl;
+			cin >> elem;
+			if (L.is_empty()) L.push_back(elem);
+			else L.push_back1(elem);
+		}
+		if (ord == 3) {
+			cout << "Какой элемент в какую позицию вы желаете добавить?" << endl;
+			cin >> elem >> index;
+			cout << endl;
+			L.push_index(index, elem);
+		}
+		if (ord == 4) {
+			cout << "Введите значение элемента, который вы желаете удалить." << endl;
+			cin >> elem;
+			L.remove(elem);
+		}
+		if (ord == 5) {
+			cout << "Введите позицию элемента, который вы желаете удалить." << endl;
+			cin >> index;
+			L.remove_index(index);
+		}
+		if (ord == 6) {
+			cout << "Введите значение элемента, который вы желаете найти." << endl;
+			cin >> elem;
+			L.find_value(elem);
+		}
+		if (ord == 7) {
+			cout << "Введите позицию элемента, который вы желаете найти." << endl;
+			cin >> index;
+			L.find_index(index);
+		}
+		if (ord == 8) {
+			L.print();
+		}
+	}
+	return 0;
+}
---
+++ lab1.h
@@ -0,0 +1,211 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int val;
+	Node* next;
+	Node(int _val) : val(_val), next(nullptr) {}
+};
+
+class TList
+{
+protected:
+	Node* first;
+	Node* last;
+
+public:
+	TList() : first(nullptr), last(nullptr) {}
+	
+	bool is_empty() {
+		return first == nullptr;
+	}
+
+	void push_back(int _val) {
+		Node* p = new Node(_val);
+		if (is_empty()) {
+			first = p;
+			last = p;
+			return;
+		}
+	}
+
+	void push_back1(int _val) {
+		Node* p = new Node(_val);
+		if (is_empty()) {
+			first = p;
+			last = p;
+			return;
+		}
+		last->next = p;
+		last = p;
+	}
+
+	void push(int _val) {
+		Node* p = new Node(_val);
+		if (is_empty()) {
+			first = p;
+			last = p;
+			return;
+		}
+	}
+
+	Node* find_value(int _val) {
+		Node* p = first;
+		while ((p != nullptr) && (p->val != _val)) p = p->next;
+		return ((p != nullptr) && (p->val == _val)) ? p : nullptr;
+
+	}
+
+	Node* find_index(int _val) {
+		Node* p = first;
+		while ((p != nullptr) && (p->val != _val)) p = p->next;
+		return ((p != nullptr) && (p->val == _val)) ? p : nullptr;
+
+	}
+
+	void push1(int _val) {
+		Node* p = new Node(_val);
+		p->next = first;
+		first = p;
+	}
+
+	void remove(int _val) {
+		if (is_empty()) return;
+		if (first->val == _val) {
+			Node* p = first; //указатель на узел равен первому указателю. 
+			first = p->next; //поле фёрст у листа становится равным полю некст у объекта пэ. пэ — узел
+			delete p;
+			return;
+		}
+		else if (last->val == _val) {
+			if (first == last) {
+				if (is_empty()) return;
+				Node* p = first;
+				first = p->next;
+				delete p;
+				return;
+			}
+			Node* p = first;
+			while (p->next != last) p = p->next;
+			p->next = nullptr;
+			delete last;
+			last = p;
+			return;
+		}
+		Node* prev = first;
+		Node* forw = first->next; 
+		while (forw != nullptr && forw->val != _val) {
+			forw = forw->next; //переходим к следующему узлу по указателю
+			prev = prev->next;
+		}
+		if (forw == nullptr) {
+			cout << "Элемента не существует!" << endl;
+			return;
+		}
+		prev->next = forw->next;
+		delete forw;
+	}
+
+	int operator[] (const int index) {
+		if (is_empty()) return 0;
+		Node* p = first;
+		for (int i = 0; i < index; i++) {
+			p = p->next;
+			if (p == nullptr) return 0;
+		}
+		return p->val; //возвращаем значение вал узла пэ
+	}
+
+	void push_index(const int index, int _val) {
+		if (is_empty()) return;
+		if (index == 0)
+		{
+			Node* p = new Node(_val);
+			p->next = first;
+			first = p;
+		}
+		else{
+		int counted = 0;
+		Node* counter = first;
+		while (counter != nullptr)
+		{
+			counter = counter->next;
+			counted++;
+		}
+		if (index == counted)
+		{
+			Node* p = new Node(_val);
+			last->next = p; //поменял у конкретного последнего объекта
+			last = p; //говорим всему листу что ласт = пэ
+		}
+		else {
+			int i = 0;
+			Node* p = new Node(_val);
+			Node* prev = first;
+			Node* forw = first->next;
+			while (forw != nullptr && i < index - 1) {
+				forw = forw->next;
+				prev = prev->next;
+				i++;
+			}
+			if (forw == nullptr) {
+				cout << "Элемент невозможно вставить в данную позицию!" << endl;
+				return;
+			}
+			prev->next = p;
+			p->next = forw;
+		}
+	}
+}
+
+	void remove_index(const int index)
+	{
+		if (index == 0)
+			first = first->next;
+		else {
+			Node* p = first;
+			int count = 0;
+			while (p != nullptr)
+			{
+				p = p->next;
+				count++;
+			}
+			if (index == count) {
+				Node* p = first;
+				while (p->next != last) p = p->next;
+				p->next = nullptr;
+				delete last;
+				last = p;
+			}
+			else {
+				int i = 0;
+				Node* prev = first;
+				Node* forw = first->next;
+				while (forw != nullptr && i < index-1) {
+					forw = forw->next;
+					prev = prev->next;
+					i++;
+				}
+				if (forw == nullptr) {
+					cout << "Элемента не существует!" << endl;
+					return;
+				}
+				prev->next = forw->next;
+				delete forw;
+			}
+		}
+	}
+
+	void print() {
+		if (is_empty()) return;
+		Node* p = first;
+		while (p != nullptr) { 
+			cout << p->val << " ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+
+};
